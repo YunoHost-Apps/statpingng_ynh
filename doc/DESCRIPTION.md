5d8af26025048460e8799422794093397e631752
oiseauroch <contact@oiseauroch.fr>
2023-01-02 15:47:57 +0100

    package application

--- doc/DESCRIPTION.md
+++ doc/DESCRIPTION.md
@@ -1,9 +1,8 @@
-Some long and extensive description of what the app is and does, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+An open source server to monitor your web applications and all other HTTP, TCP, UDP, ICMP and gRPC services.
 
 ### Features
 
-- Ut enim ad minim veniam, quis nostrud exercitation ullamco ;
-- Laboris nisi ut aliquip ex ea commodo consequat ;
-- Duis aute irure dolor in reprehenderit in voluptate ;
-- Velit esse cillum dolore eu fugiat nulla pariatur ;
-- Excepteur sint occaecat cupidatat non proident, sunt in culpa."
+- Service Monitoring
+- Notifications
+- Mobile app
+
--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,7 +3,7 @@
 #=================================================
 # COMMON VARIABLES
 #=================================================
-
+pkg_dependencies="postgresql"
 #=================================================
 # PERSONAL HELPERS
 #=================================================
@@ -15,7 +15,7 @@ get_ip() {
   curl ip.me
 }
 # inspired by restic helper
-install_garage () {
+install_statping () {
   architecture=$(uname -m)
   arch=''
   case $architecture in
@@ -37,8 +37,8 @@ install_garage () {
       ;;
   esac
   wget https://github.com/statping-ng/statping-ng/releases/download/v$STATPING_VERSION/statping-linux-$arch.tar.gz 2>&1 >/dev/null
-  tar zxf statping.tar.gz
-  rm statping.tar.gz
+  tar zxf statping-linux-$arch.tar.gz
+  rm statping-linux-$arch.tar.gz
   chmod +x statping
 }
 
