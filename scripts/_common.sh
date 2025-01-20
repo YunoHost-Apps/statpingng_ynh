e67a44d80249eb67842ced2af41ac7b5addb1d94
eric_G <46165813+ericgaspar@users.noreply.github.com>
2025-01-20 21:15:00 +0100

    cleaning

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,7 +3,7 @@
 #=================================================
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
-#REMOVEME? pkg_dependencies="postgresql"
+
 STATPING_VERSION="0.90.80"
 
 get_ip() {
